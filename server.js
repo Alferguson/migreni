35d6d7892d0cdfdda855815816388481ed4f8ee4
Alferguson <alferguson@berkeley.edu>
2017-11-28 11:38:32 -0800

    for controllers

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // Routes
 // =============================================================
-require("./routes/api-routes.js")(app);
+require("./controllers")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
