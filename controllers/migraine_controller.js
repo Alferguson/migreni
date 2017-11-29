fcef1c4e6a940dc487f23329857a7ce31dcedf3a
Alferguson <alferguson@berkeley.edu>
2017-11-28 18:29:08 -0800

    user controller changes

--- controllers/migraine_controller.js
+++ controllers/migraine_controller.js
@@ -3,7 +3,7 @@ var db = require("../models");
 module.exports = function(app) {
 
   // GET route to display all migraine data when user clicks "Display previous migraines"
-  app.get("/migraines/:id", function(req, res) {
+  app.get("/migraines/:uuid", function(req, res) {
     db.Migraine.findAll({
       // display all data where user uuid = database uuid
       where: {
@@ -18,8 +18,9 @@ module.exports = function(app) {
       res.json(dbMigraine).render("index", { migraine: dbMigraine });
     });
   });
+
   // POST route to create new migraines when user clicks submit
-  app.post("/api/migraines", function(req, res) {
+  app.post("/migraines", function(req, res) {
     // grab data from 4 questions, WHAT DO ABOUT MEDS AND WEATHER???
     db.Migraine.create({
       intensity: req.body.intensity,
@@ -27,18 +28,38 @@ module.exports = function(app) {
       acute_meds: req.body.acute_meds,
       triggers: req.body.triggers
     }).then(function(dbMigraine) {
+      // HOW TO target user ID
       res.json(dbMigraine);
     });
   });
 
-  app.delete("/api/migraines/:id", function(req, res) {
+  // PUT route to update previous migraines
+  app.put("/migraines/:uuid", function(req, res) {
+    db.Migraine.update ({
+      intensity: req.body.intensity,
+      chronic_meds: req.body.chronic_meds,
+      acute_meds: req.body.acute_meds,
+      triggers: req.body.triggers
+      }, {    
+      where: {
+        uuid: req.params.uuid
+      }
+    }).then(function(results) {
+      res.json(results);
+
+    }).catch(function(err) {
+      res.json(err);
+    });
+  });  
+
+  // to delete previous migraines
+  app.delete("/migraines/:uuid", function(req, res) {
     db.Migraine.destroy({
       where: {
-        id: req.params.id
+        uuid: req.params.uuid
       }
     }).then(function(dbMigraine) {
       res.json(dbMigraine);
     });
   });
-
 };
---
+++ controllers/user_controller.js
@@ -0,0 +1,45 @@
+var db = require("../models");
+
+module.exports = function(app) {
+
+  // GET route to display user name at user's page
+  app.get("/user/:uuid", function(req, res) {
+    db.User.findOne({
+    	// display user_name
+    	user_name: user_name
+      // display all data where user uuid = database uuid
+      where: {
+        uuid: req.params.uuid
+      },
+    }).then(function(dbUser) {
+      // display on handlebars, may not work
+      res.json(dbUser).render("index", { user: dbUser });
+    });
+  });
+
+  // POST route to create new users new account is set up with google OAuth
+  app.post("/user", function(req, res) {
+    // grab data from 4 questions, WHAT DO ABOUT MEDS AND WEATHER???
+    db.Migraine.create({
+      user_name: req.body.user_name,
+      sex: req.body.sex,
+      age: req.body.age,
+      // maybe birthday instead to calculate age
+      location: req.body.location
+    }).then(function(dbUser) {
+      // HOW TO target user ID
+      res.json(dbUser);
+    });
+  });
+
+  // to delete user account 
+  app.delete("/user/:uuid", function(req, res) {
+    db.User.destroy({
+      where: {
+        uuid: req.params.uuid
+      }
+    }).then(function(dbUser) {
+      res.json(dbUser);
+    });
+  });
+};
