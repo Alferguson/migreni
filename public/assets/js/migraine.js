dce2658e76ffe40c3e742b2a56af0101ce47d93c
Alferguson <alferguson@berkeley.edu>
2017-12-01 10:07:10 -0800

    changes to migraine.js front end

--- public/assets/js/migraine.js
+++ public/assets/js/migraine.js
@@ -12,6 +12,7 @@ $(document.body).ready(function() {
   // In localhost:8080/user/id
   var userId = url.split("user/")[1];
 
+  // calendar stuff
   $(".calendar").pignoseCalendar();
   $(".date-visibility").hide();
   $(".meds-1-visibility").hide();
@@ -35,6 +36,7 @@ $(document.body).ready(function() {
       else $(".meds-2-visibility").hide();
   });
 
+  // weather call
   var currentWeather = {};
   if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
@@ -63,48 +65,36 @@ $(document.body).ready(function() {
       });
   }
 
+  // on submit btn click
   $("#submit").on("click", function() {
 
+    // to grab dates
     var dateVal = $("#date-val").val().trim();
     if (dateVal === "") dateVal = moment().format('YYYY-MM-DD');
+
+    // to grab migraine data
     var migraines = {
-        intensity: $("#intensity-val").val().trim(),
-        location: $("#weather-city").text(),
-        date: dateVal,
-        trigger: $("#trigger-val").val().trim()
+      intensity: $("#intensity-val").val().trim(),
+      location: $("#weather-city").text(),
+      date: dateVal,
+      trigger: $("#trigger-val").val().trim()
     };
-    console.log(migraines);
-    $.ajax("/api/migraines/" + userId, {
-        type: "POST",
-        data: migraines
-    }).then(function(resultMigraine) {
-        console.log(resultMigraine);
-        var migraineId = resultMigraine.id
-        $.ajax("/api/weather/" + migraineId, {
-            type: "POST",
-            data: currentWeather
-        }).done(function(resultWeather) {
-            console.log(resultWeather);
-        });
-    });
-
-
 
 // put inside first ajax call
     var chronicTreatment = {
         name: $("#chronic-treatment").val().trim(),
         acute: false,
         dose: {
-            value: $("#chronic-dosage").val().trim()
+          value: $("#chronic-dosage").val().trim()
         }
     }
     // TODO
     // if function for if the user didn't enter anything for chronic treatment, get last instance of chronic treatment
     if(chronicTreatment.name === null) {
         $.ajax("/api/treatments/" + userId, {
-            type: "GET"
+          type: "GET"
         }).then(function(chronicTreatment) {
-            // afsdaf
+          // afsdaf
         })
     }
 
@@ -112,78 +102,130 @@ $(document.body).ready(function() {
         name: $("#acute-treatment").val().trim(),
         acute: true,
         dose: {
-            value: $("#acute-dosage").val().trim()
+          value: $("#acute-dosage").val().trim()
         }
     }
 
-    // POST a new chronic treatment and acute treatment
-    $.ajax("/api/treatments/" + userId, {
+
+
+
+
+    console.log(migraines);
+    $.ajax("/api/migraines/" + userId, {
         type: "POST",
-        data: chronicTreatment
-    }).then(function(resultChronicTreatment) {
-        console.log(resultChronicTreatment);
-        $.ajax("/api/dose/" + userId, {
+        data: migraines
+
+
+
+    }).then(function(resultMigraine) {
+      console.log(resultMigraine);
+      var migraineId = resultMigraine.id;
+
+
+
+      // weather call to store weather
+      $.ajax("/api/weather/" + migraineId, {
+        type: "POST",
+        data: currentWeather
+      }).done(function(resultWeather) {
+        console.log(resultWeather);
+
+
+
+        // POST a new chronic treatment and acute treatment
+        $.ajax("/api/treatments/" + userId, {
+          type: "POST",
+          data: chronicTreatment
+        }).then(function(resultChronicTreatment) {
+          console.log(resultChronicTreatment);
+
+
+          // POST chronic dose
+          $.ajax("/api/dose/" + userId, {
             type: "POST",
             data: chronicTreatment.dose
-        }).then(function(doseResult) {
+          }).then(function(doseResult) {
             console.log(doseResult);
-            // acute treatment call
-            $.ajax("/api/treatments/" + userId, {
-                type: "POST",
-                data: acuteTreatment
-            }).then(function(resultAcuteTreatment) {
-                console.log(resultAcuteTreatment);
-                // acute treatment call
-                $.ajax("/api/dose/" + userId, {
-                    type: "POST",
-                    data: acuteTreatment.dose
-                }).then(function(doseResult) {
-                    console.log(doseResult);
-                })
-            })
-        })
-    });
-});
-    // END OF SUBMIT
 
-// FUNCTION TO GET MIGRAINE DATA AND SHOW IT
-function createMigraineData(migraineData) {
-  console.log("author data", migraineData)
-  var newTr = $("<tr>");
-  newTr.data("author", migraineData);
-  newTr.append("<td>" + migraineData.date + "</td>");
-  newTr.append("<td> " + migraineData.intensity + "</td>");
-  newTr.append("<td>" + migraineData.trigger + "</td>");
-  newTr.append("<td><a href='/cms?author_id=" + migraineData.id + "'>Create a Post</a></td>");
-  newTr.append("<td><a style='cursor:pointer;color:red' class='delete-author'>Delete Migraine</a></td>");
-  return newTr;
-}  
+
+
+            // POST acute treatment 
+            $.ajax("/api/treatments/" + userId, {
+              type: "POST",
+              data: acuteTreatment
+            }).then(function(resultAcuteTreatment) {
+              console.log(resultAcuteTreatment);
+
+
+
+              // POST acute dose
+              $.ajax("/api/dose/" + userId, {
+                type: "POST",
+                data: acuteTreatment.dose
+              }).then(function(doseResult) {
+                console.log(doseResult);
+              })
+            })
+          })
+        })
+      });
+    });
+  });
+  // END OF SUBMIT
+
+  // FUNCTION TO GET MIGRAINE DATA AND SHOW IT
+  function getMigraineData(migraineData) {
+    console.log("migraine data", migraineData)
+    var newTr = $("<tr>");
+    newTr.data("author", migraineData);
+    newTr.append("<td>" + migraineData.date + "</td>");
+    newTr.append("<td> " + migraineData.intensity + "</td>");
+    newTr.append("<td>" + migraineData.trigger + "</td>");
+    newTr.append("<td><a href='/cms?author_id=" + migraineData.id + "'>Create a Post</a></td>");
+    newTr.append("<td><a style='cursor:pointer;color:red' class='delete-author'>Delete Migraine</a></td>");
+    return newTr;
+  };
 
   // show all migraine and assoicated data
   $("#show").on("click", function() {
-      $.get("/api/migraines/:id" + userId, function(data) {
-          var migraineData = [];
-          migraineData.push(createMigraineData(migraineData));
-      })
-  }    
+    $.get("/api/migraines/:id" + userId, function(data) {
+      var migraineData = [];
+      migraineData.push(getMigraineData(migraineData));
+    })
+  });    
 
-});
 
-// if update button is clicked 
-$("#update").on("click", function() {
-  updating = true;
-  // TODO, change update button to don't update
-  // function to update previous migraines
-  if (updating === true) {
-    function updateMigraine(migraine) {
+
+  // if UPDATE button is clicked 
+  $("#update").on("click", function() {
+    updating = true;
+    // TODO, change update button to don't update
+    // function to update previous migraines
+    if (updating === true) {
+      function updateMigraine(migraine) {
+        $.ajax({
+          method: "PUT",
+          url: "/api/migraines/" + userId,
+          data: migraine
+        }).done(function() {
+          // set updating to false
+          updating = false
+        })
+      }
+    }  
+  })
+
+
+  // if DELETE button is clicked 
+  $("#delete").on("click", function() {
+    function deleteMigraine(migraine) {
       $.ajax({
-        method: "PUT",
-        url: "/api/migraines" + userId,
+        method: "DELETE",
+        url: "/api/migraines/" + userId,
         data: migraine
       }).done(function() {
-        // set updating to false
-        updating = false
+        console.log("It has been deleted");
       })
     }
-  }  
-}
+  })
+})
