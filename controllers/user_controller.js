3687dba650c04b3f5160aabeb282b54f6c7268d9
asajbel <serdan66@gmail.com>
2017-12-02 12:04:07 -0800

    Login should work as it was doing asyc things

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -104,7 +104,7 @@ router.post("/register", [
           if (err) throw err;
           console.log("logged in " + req.user.id);
           console.log("req.user name " + req.user.username);
-          return res.redirect("/user/" + req.user.id);
+          res.redirect("/loginSuccess");
         });
       }).catch(function(err) {
 
@@ -118,7 +118,9 @@ router.get("/login", function(req, res) {
 });
 
 router.get("/loginSuccess", function(req, res) {
-  res.redirect("/user/" + req.user.id);
+  req.session.save(function() {
+    res.redirect("/user/" + req.user.id);
+  });
 });
 
 router.post("/login",
@@ -129,8 +131,9 @@ router.post("/login",
 
 router.get("/logout", function(req, res) {
   req.logout();
-  req.session.destroy();
-  res.redirect("/");
+  req.session.destroy(function() {
+    res.redirect("/");
+  });
 });
 
 passport.serializeUser((user_id, done) => {
