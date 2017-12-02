87ba339d286120f2d6286b780f7460868f800230
sameen63 <sameen63@gmail.com>
2017-12-02 13:31:56 -0800

    form validation front end

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -6,13 +6,43 @@ $(document.body).ready(function() {
       var password = document.forms["signup"]["password"].value;
       var password2 = document.forms["signup"]["passwordMatch"].value;
       var email = document.forms["signup"]["email"].value;
-      var age = document.forms["signup"]["email"].value;
+      var age = document.forms["signup"]["age"].value;
       var gender = document.forms["signup"]["gender"].value;
       var location = document.forms["signup"]["location"].value;
 
-      if (password !== password2) {
-        $("#passwordMismatch").show();
-        return false;
+      //Password validation
+        if (password == "") {
+          $("#noPassword").show();
       }
-    });
+      // Password match
+        if (password !== password2) {
+          $("#passwordMismatch").show();
+          return false;
+      }
+
+      //Age validation
+        if (isNaN(age) || age < 1 || age > 100) { 
+         $("#wrongAge").show();
+          // return false;
+
+      }
+
+        var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        //Email validation
+        if (!filter.test(email)) {
+        $("#wrongEmail").show();
+        //return false;
+          }
+
+        //Username can not be null
+        if (username == "") {
+          $("#usernameNull").show();
+          //return false;
+        }
+        if (location == "") {
+          $("#noLocation").show();
+          //return false;
+      }
+    }
+  );
 })
