49eeea4d4a13e4d68c5e07d75cd74fd825907218
asajbel <serdan66@gmail.com>
2017-12-02 14:39:04 -0800

    Fixed front end validation so it displays multiple errors and stops submission if there are any

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -3,6 +3,7 @@ $(document.body).ready(function() {
   $(".error").hide();
   $("#signup").on("submit",
     function validateRegistration() {
+      $(".error").hide();
       var username = document.forms["signup"]["username"].value;
       var password = document.forms["signup"]["password"].value;
       var password2 = document.forms["signup"]["passwordMatch"].value;
@@ -10,39 +11,49 @@ $(document.body).ready(function() {
       var age = document.forms["signup"]["age"].value;
       var gender = document.forms["signup"]["gender"].value;
       var location = document.forms["signup"]["location"].value;
+      var fine = true;
 
       //Password validation
-        if (password == "") {
-          $("#noPassword").show();
+      if (password == "") {
+        $("#noPassword").show();
+        fine = false;
       }
+
+      if (password.length < 8) {
+        $("#shortPassword").show();
+        fine = false;
+      }
+      
       // Password match
-        if (password !== password2) {
-          $("#passwordMismatch").show();
-          return false;
+      if (password !== password2) {
+        $("#passwordMismatch").show();
+        fine = false;
       }
 
       //Age validation
-        if (isNaN(age) || age < 1 || age > 100) { 
-         $("#wrongAge").show();
-          // return false;
+      if (isNaN(age) || age < 1 || age > 100) {
+        $("#wrongAge").show();
+        fine = false;
 
       }
 
-        var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-        //Email validation
-        if (!filter.test(email)) {
+      var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      //Email validation
+      if (!filter.test(email)) {
         $("#wrongEmail").show();
-        //return false;
-          }
-
-        //Username can not be null
-        if (username == "") {
-          $("#usernameNull").show();
-          //return false;
-        }
-        if (location == "") {
-          $("#noLocation").show();
-          //return false;
+        fine = false;
       }
+
+      //Username can not be null
+      if (username == "") {
+        $("#usernameNull").show();
+        fine = false;
+      }
+      if (location == "") {
+        $("#noLocation").show();
+        fine = false;
+      }
+
+      return fine; 
     });
 });
