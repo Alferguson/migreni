8bdf665d33ad191d5141ea1b942cd22575591601
Alferguson <alferguson@berkeley.edu>
2017-12-02 15:32:45 -0800

    conditional for previous chronic treatment fixed

--- controllers/migraine_controller.js
+++ controllers/migraine_controller.js
@@ -66,37 +66,33 @@ router.post("/api/migraines/:id", function(req, res) {
     }).then(function(dbWeather) {
       // res.json(dbWeather);
     });  
-    // if function to not POST chronic treatment data if chronic treatment is not entered
-    if (req.body.chronicTreatment.treatment_name === "" && req.body.acuteTreatment.treatment_name === "") {
-      return;
-    } else {
-      // to ADD chronic treatment
-      db.Treatment.create({
-        treatment_name: req.body.chronicTreatment.treatment_name,
-        acute: false,
-        dose: req.body.chronicTreatment.dose,
-        dose_unit: req.body.chronicTreatment.dose_unit   
-      }).then(function(dbChronicTreatment) {
-        // to ADD keys to MigraineTreatment for chronic
-        db.MigraineTreatment.create({
-          TreatmentId: dbChronicTreatment.id,
-          MigraineId: dbMigraine.id
-        })
-      });
-      // to ADD acute treatment
-      db.Treatment.create({
-        treatment_name: req.body.acuteTreatment.treatment_name,
-        acute: true,
-        dose: req.body.acuteTreatment.dose,
-        dose_unit: req.body.acuteTreatment.dose_unit  
-      }).then(function(dbAcuteTreatment) {
-        // to ADD keys to MigraineTreatment for acute
-        db.MigraineTreatment.create({
-          TreatmentId: dbAcuteTreatment.id,
-          MigraineId: dbMigraine.id
-        })
-      });
-    }
+
+    // to ADD chronic treatment
+    db.Treatment.create({
+      treatment_name: req.body.chronicTreatment.treatment_name,
+      acute: false,
+      dose: req.body.chronicTreatment.dose,
+      dose_unit: req.body.chronicTreatment.dose_unit   
+    }).then(function(dbChronicTreatment) {
+      // to ADD keys to MigraineTreatment for chronic
+      db.MigraineTreatment.create({
+        TreatmentId: dbChronicTreatment.id,
+        MigraineId: dbMigraine.id
+      })
+    });
+    // to ADD acute treatment
+    db.Treatment.create({
+      treatment_name: req.body.acuteTreatment.treatment_name,
+      acute: true,
+      dose: req.body.acuteTreatment.dose,
+      dose_unit: req.body.acuteTreatment.dose_unit  
+    }).then(function(dbAcuteTreatment) {
+      // to ADD keys to MigraineTreatment for acute
+      db.MigraineTreatment.create({
+        TreatmentId: dbAcuteTreatment.id,
+        MigraineId: dbMigraine.id
+      })
+    });
     res.end();   
   })
 });   
--- public/assets/js/migraine.js
+++ public/assets/js/migraine.js
@@ -70,8 +70,6 @@ $(document.body).ready(function() {
   $.get("/api/migraines1/" + userId, function(chronicTreatment) {
     ctn = chronicTreatment.Treatments[0].treatment_name;
     ctd = chronicTreatment.Treatments[0].dose;
-    console.log(ctn);
-    console.log(ctd);
   });
 
   // on submit btn click
@@ -86,22 +84,21 @@ $(document.body).ready(function() {
       intensity: $("#intensity-val").val() == undefined ? '' : $("#intensity-val").val().trim(),
       location: $("#weather-city").text(),
       date: dateVal,
-      trigger: $("#trigger-val").val() == undefined ? '' : $("#trigger-val").val().trim(),
+      trigger: $("#trigger-val").val() == "" ? '' : $("#trigger-val").val().trim(),
       currentWeather,
       chronicTreatment: {
-        treatment_name: $("#chronic-treatment").val() == undefined ? ctn : $("#chronic-treatment").val().trim(),
+        treatment_name: $("#chronic-treatment").val() == "" ? ctn : $("#chronic-treatment").val().trim(),
         acute: false,
-        dose: $("#chronic-dosage").val() == undefined ? ctd : $("#chronic-dosage").val().trim(),
+        dose: $("#chronic-dosage").val() == "" ? ctd : $("#chronic-dosage").val().trim(),
         dose_unit: "mg"
       },
       acuteTreatment: {
-        treatment_name: $("#acute-treatment").val() == undefined ? '' : $("#acute-treatment").val().trim(),
+        treatment_name: $("#acute-treatment").val() == "" ? 'N/A' : $("#acute-treatment").val().trim(),
         acute: true,
-        dose: $("#acute-dosage").val() == undefined ? '' : $("#acute-dosage").val().trim(),
+        dose: $("#acute-dosage").val() == "" ? 0 : $("#acute-dosage").val().trim(),
         dose_unit: "mg"
       }
     };
-
 
     // POST new migraine and assoicated data
     $.ajax("/api/migraines/" + userId, {
@@ -110,8 +107,9 @@ $(document.body).ready(function() {
     }).then(function(resultMigraine) {
       console.log("Migraine data has been logged");
     });
+  // END OF SUBMIT  
   });  
-  // END OF SUBMIT
+ 
 
   // // FUNCTION TO GET MIGRAINE DATA AND SHOW IT
   // function getMigraineData(migraineData) {
